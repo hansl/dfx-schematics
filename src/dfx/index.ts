c037e884b88eda1633bf01ebd4896371e20da68d
Hans Larsen <hans@larsen.online>
2020-02-09 22:01:47 -0800

    update package.json

--- src/dfx/index.ts
+++ src/dfx/index.ts
@@ -16,6 +16,8 @@ export function dfx(options: any): Rule {
           }),
         ]),
       ),
+
+      // Update gitignore.
       (tree) => {
         if (!tree.exists("/.gitignore")) {
           tree.create("/.gitignore", "");
@@ -31,6 +33,15 @@ canisters/
 `;
         tree.overwrite("/.gitignore", gitignore);
       },
+
+      // Update package.json
+      (tree) => {
+        const packageJson = (tree.read("/package.json") || "").toString();
+        const json = JSON.parse(packageJson);
+
+        json["scripts"]["build"] = "mkdir -p canisters/hello/assets; ng build --prod && cat dist/dfx-demo/index.html | grep -oE '[a-z0-9.-]*.js' | grep 2015 | sed s%^%dist/dfx-demo/% | xargs cat > canisters/hello/assets/index.js";
+        tree.overwrite("/package.json", JSON.stringify(json));
+      }
     ]);
   };
 }
