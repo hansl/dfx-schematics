0efceb855d3febc2b26ac9cdff29cd75ab6e8e4c
Hans Larsen <hans@larsen.online>
2020-02-11 20:53:02 -0800

    ready for meetup

--- src/dfx/index.ts
+++ src/dfx/index.ts
@@ -36,15 +36,35 @@ canisters/
 
       // Update package.json
       (tree) => {
-        const packageJson = (tree.read("/package.json") || "").toString();
+        const packageJson = (tree.read("/package.json") !).toString();
         const json = JSON.parse(packageJson);
+        const angularJsonContent = (tree.read("/angular.json") !).toString();
+        const angularJson = JSON.parse(angularJsonContent);
+
+        const angularProjectName = Object.keys(angularJson['projects'])[0] !;
+
 
         json["scripts"]["build"] = `mkdir -p canisters/${options.projectName}/assets; `
-                                 + `ng build --prod && cat dist/dfx-demo/index.html `
+                                 + `ng build --prod && cat dist/${angularProjectName}/index.html `
                                  + `| grep -oE '[a-z0-9.-]*.js' | grep 2015 `
-                                 + `| sed s%^%dist/dfx-demo/% `
+                                 + `| sed s%^%dist/${angularProjectName}/% `
                                  + `| xargs cat > canisters/${options.projectName}/assets/index.js`;
-        tree.overwrite("/package.json", JSON.stringify(json));
+        tree.overwrite("/package.json", JSON.stringify(json, null, 4));
+      },
+
+      // Update tsconfig.json
+      (tree) => {
+        const tsConfigJson = (tree.read("/tsconfig.json") !).toString();
+        const json = JSON.parse(tsConfigJson);
+
+        json['compilerOptions']['paths'] = Object.assign(json['compilerOptions']['paths'] || {}, {
+          "ic:userlib": [
+            `${process.env['HOME']}/.cache/dfinity/versions/0.5.0/js-user-library/dist/lib.prod.js`
+          ],
+          "ic:canisters/*": ["canisters/*/main.js"],
+          "ic:idl/*": ["canisters/*/main.did.js"]
+        });
+        tree.overwrite("/tsconfig.json", JSON.stringify(json, null, 4));
       }
     ]);
   };
